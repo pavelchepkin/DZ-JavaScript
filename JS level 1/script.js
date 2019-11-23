33c0471b91fe4f02a192921a68261b25501873bb
Pavel Chepkin <pavel.chepkin@gmail.com>
2019-11-24 01:28:56 +0300

    первое Дз

---
+++ JS level 1/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var cels = +prompt('Введите температуру в цельсиях:');
+var farengeit = (9 / 5) * cels + 32;
+    alert(cels + " в градусах Цельсия равняется " + farengeit + " в градусах Фаренгейт");
+
+
+var name = prompt("Введите имя (например Василий)");
+var admin = name;
+alert('admin ' + admin);
+
+var sum = 1000 + "108";
+alert('В результате записи "1000 + "108" получим ' + sum);
+
