7c1dd6eea0c4c01b222287e4e7e90f591ea931c0
Pavel Chepkin <pavel.chepkin@gmail.com>
2019-12-08 19:17:36 +0300

    Доделал ДЗ

--- JS level 1/lesson_4.js
+++ JS level 1/lesson_4.js
@@ -1,6 +1,5 @@
 'use strict';
 var level = 1;
-// занание 1
 console.log('занание ' + level++ + ' =>');
 
 let a = Math.round(Math.random() * 1200);
@@ -42,48 +41,75 @@ function numberToObject(a) {
 numberToObject(a);
 
 // ------------------------------------------------------------------------------------------------------------
-// занание 2
 console.log('занание ' + level++ + ' =>');
 
 const basket = [
     {
         idProduct: 48,
-        name: 'jacket',
+        name: 'куртка',
         price: 56,
         quantity: 2
     }
     ,
     {
         idProduct: 49,
-        name: 'skirt',
+        name: 'рубашка',
         price: 35,
         quantity: 5
     }
     ,
     {
         idProduct: 50,
-        name: 'pants',
+        name: 'шорты',
         price: 23,
         quantity: 3
     }
     ,
     {
         idProduct: 51,
-        name: 'shoes',
+        name: 'ботинки',
         price: 47,
         quantity: 1
     }
 ];
-function basketPrise (basket) {
+
+function basketPrise(basket) {
     let totalPrise = 0;
-    for(let i = 0; i < basket.length; i++){
-        totalPrise += basket[i].price * basket[i].quantity
-        console.log(basket[i].name + ' по цене ' + basket[i].price + ' в колличестве ' + basket[i].quantity + ' штук')
+    for (let i = 0; i < basket.length; i++) {
+        totalPrise += basket[i].price * basket[i].quantity;
+        console.log(basket[i].name + ' по цене ' + basket[i].price + ' в количестве ' + basket[i].quantity + ' штук')
     }
     return totalPrise;
 }
-    console.log('Товаров на сумму: ' + basketPrise (basket));
+
+console.log('Товаров на сумму: ' + basketPrise(basket));
 
 // ------------------------------------------------------------------------------------------------------------
-// занание 2
 console.log('занание ' + level++ + ' =>');
+
+const globalProduct = {
+    name:'',
+    idProduct:'',
+    quantity:'',
+    possibleSize: ['S', 'M', 'L', 'XL', 'XXL'],
+    size:'',
+    possibleColor: ['black', 'white', 'red'],
+    color:'',
+    possibleMaterial: ['cotton', 'fleece', 'elastane'],
+    material:'',
+    price:'',
+    discount:'',
+    promoCode:'',
+};
+
+// Также раз в интернет магазине есть доставка, подумал и об этом
+const adress = {
+    zipAdress:'',
+    country:'',
+    city:'',
+    street:'',
+    house:'',
+    flat:'',
+};
+console.log(globalProduct);
+console.log(adress);
