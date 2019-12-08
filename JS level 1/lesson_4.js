73ed1c25d3afc1a61929826a01f9c61656a483fa
Pavel Chepkin <pavel.chepkin@gmail.com>
2019-12-08 18:05:52 +0300

    Переделал :-) задание 1 Сделал задание 2

--- JS level 1/lesson_4.js
+++ JS level 1/lesson_4.js
@@ -1,35 +1,89 @@
 'use strict';
+var level = 1;
+// занание 1
+console.log('занание ' + level++ + ' =>');
 
-let a = Math.round(Math.random()*1100);
-console.log(a);
+let a = Math.round(Math.random() * 1200);
+console.log('Числo ' + a);
+
+const objekt = {};
 
 function numberToObject(a) {
     if (a <= 999 && a > 0) {
         let b = {
             hundreds() {
-                let hundred = Math.floor(a/100);
-                return hundred;
+                return Math.floor(a / 100);
             },
             dozens() {
-                let dozen = Math.floor((a-(hundred*100))/10);
-                return dozen;
+                return Math.floor((a - (hundred * 100)) / 10);
             },
             units() {
-                let unit = a-((hundred*100)+(dozen*10));
-                return unit;
+                return a - ((hundred * 100) + (dozen * 10));
             }
         };
+
         var hundred = b.hundreds();
         var dozen = b.dozens();
         var unit = b.units();
-        console.log(hundred);
-        console.log(dozen);
-        console.log(unit);
+
+        objekt.hundred = hundred;
+        objekt.dozen = dozen;
+        objekt.unit = unit;
+
+        console.log(objekt);
+
     } else {
-        console.log('Число превышает 999');
-        let b = {};
-        console.log(b);
-    };
-};
+        console.log('Число ' + a + ' превышает 999');
+        console.log(objekt);
+    }
+
+}
 
 numberToObject(a);
+
+// ------------------------------------------------------------------------------------------------------------
+// занание 2
+console.log('занание ' + level++ + ' =>');
+
+const basket = [
+    {
+        idProduct: 48,
+        name: 'jacket',
+        price: 56,
+        quantity: 2
+    }
+    ,
+    {
+        idProduct: 49,
+        name: 'skirt',
+        price: 35,
+        quantity: 5
+    }
+    ,
+    {
+        idProduct: 50,
+        name: 'pants',
+        price: 23,
+        quantity: 3
+    }
+    ,
+    {
+        idProduct: 51,
+        name: 'shoes',
+        price: 47,
+        quantity: 1
+    }
+];
+function basketPrise (basket) {
+    let totalPrise = 0;
+    for(let i = 0; i < basket.length; i++){
+        totalPrise += basket[i].price * basket[i].quantity
+        console.log(basket[i].name + ' по цене ' + basket[i].price + ' в колличестве ' + basket[i].quantity + ' штук')
+    }
+    return totalPrise;
+}
+    console.log('Товаров на сумму: ' + basketPrise (basket));
+
+// ------------------------------------------------------------------------------------------------------------
+// занание 2
+console.log('занание ' + level++ + ' =>');
